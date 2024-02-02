fdb1d0b9f7bd546c3b95866763eb4ebd1e941285
Odeyalo <61063611+justJavaProgrammer@users.noreply.github.com>
2024-02-02 17:12:17 +0200

    Added sidebar component (#2)
    Added sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import MiniDrawer from './components/sidebar/Sidebar';
 import Login from "./login";
+import Search from "./components/search/Search";
+
+
+const Layout = ({children}: any) => {
+    return (
+        <div style={{display: 'flex'}}>
+            <MiniDrawer/>
+            <main style={{flexGrow: 1, padding: '20px'}}>
+                {children}
+            </main>
+        </div>
+    );
+};
+
+function Home() {
+    return (
+        <div>
+            HOME
+        </div>
+    );
+}
 
 function App() {
     return (
         <div className="App">
             <BrowserRouter>
-                <Routes>
-                    <Route path={"/login"} element={<Login/>}>
-                    </Route>
-                </Routes>
+
+                <Layout>
+                    <Routes>
+                        <Route path={"/"} element={<Home/>}>
+                        </Route>
+                        <Route path={"/search"} element={<Search/>}>
+                        </Route>
+                    </Routes>
+                </Layout>
             </BrowserRouter>
         </div>
     );
---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import {CSSObject, styled, Theme} from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MuiDrawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import CssBaseline from '@mui/material/CssBaseline';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import SidebarButtonItem from "./SidebarButtonItem";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme: Theme): CSSObject => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+});
+
+const Drawer = styled(MuiDrawer)(
+    ({theme}) => ({
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+        boxSizing: 'border-box',
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+    }),
+);
+
+export default function MiniDrawer() {
+    const open = true;
+
+    return (
+        <Box sx={{display: 'flex'}}>
+            <CssBaseline/>
+            <Drawer variant="permanent" anchor={"left"} open={open}>
+
+                <Box textAlign={"center"}>
+                    <Typography variant={"h5"} padding={"10px"}>Sonata</Typography>
+                </Box>
+
+                <Divider></Divider>
+
+                <List>
+                    <SidebarButtonItem text={"Home"} link={"/"} Icon={HomeIcon}/>
+                    <SidebarButtonItem text={"Search"} link={"/search"} Icon={SearchIcon}/>
+                </List>
+                <Divider/>
+            </Drawer>
+        </Box>
+    );
+}
